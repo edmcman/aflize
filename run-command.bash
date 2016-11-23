ce236bc2e6a8efb2046019465a8ad0ff611399dc
Edward J. Schwartz <eschwartz@cert.org>
2016-11-23 20:21:33 +0000

    now we can run commands

---
+++ run-command.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+PKG="${1?}"
+shift
+PKGVOLDIR="$(pwd)/pkg"
+FILE="${1?}"
+shift
+CMD="$@"
+
+# copy FILE to docker container
+test -f "$FILE"
+NEWFILE="$PKGVOLDIR/$(basename $FILE)"
+NEWDOCKFILE="/root/pkg/$(basename $FILE)"
+cp "$FILE" "$NEWFILE"
+
+docker run -it --rm=true -v $PKGVOLDIR:/root/pkg $PKG-image bash -i -c "${CMD//$FILE/$NEWDOCKFILE}"
---
+++ setup-image.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -ex
+
+PKG="${1?}"
+PKGVOLDIR="$(pwd)/pkg"
+
+docker rm -f $PKG-temp || true
+docker run -it --name $PKG-temp -v $PKGVOLDIR:/root/pkg aflize bash -i -c './build-and-install-coverage-pkg.sh flasm'
+docker commit $PKG-temp $PKG-image
