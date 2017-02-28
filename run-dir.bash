192d17fd23eed3ca436ba02f2a68652efe3eb4d2
Edward J. Schwartz <eschwartz@cert.org>
2017-02-28 14:08:45 -0500

    make file better for graphs

--- run-dir.bash
+++ run-dir.bash
@@ -22,14 +22,18 @@ rm /tmp/cov "$VOLDIR/cov.map" || true
 touch /tmp/cov
 lcov -z --directory "$VOLDIR"
 
+n=0
+
 for testcase in $(find "$TESTCASEDIR/.afl_seeds" "$TESTCASEDIR/.mayhem_seeds" -type f  -printf '%p %T@\n' | sort -n -k2,2 | awk '{print $1}')
 do
     #lcov -z --directory "$VOLDIR"
     timeout -k 60 60 $DIR/run-command.bash "$PKG" $testcase "$CMD" >&2
     lcov --capture --directory "$VOLDIR" | $DIR/parse-info.py | sort -u > /tmp/new
+    timestamp=$(find "$testcase" -printf '%T@\n')
     comm -13 /tmp/cov /tmp/new | while read -r l
     do
-	echo \"$testcase\",\"$l\" >> "$VOLDIR/lcov/cov.map"
+        n=$((n+1))
+	echo \"$testcase\",\"$l\",\"$n\",\"$timestamp\" >> "$VOLDIR/lcov/cov.map"
     done
     cat /tmp/cov /tmp/new | sort -u > /tmp/cov2
     mv /tmp/cov2 /tmp/cov
