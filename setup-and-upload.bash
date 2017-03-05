9d3b648c020583ce1e27ee48abdcd00d5c3339a7
Edward J. Schwartz <eschwartz@cert.org>
2017-03-05 21:02:37 +0000

    Fix script

--- setup-and-upload.bash
+++ setup-and-upload.bash
@@ -6,6 +6,6 @@ for p in $@
 do
     echo Building $p
     ./setup-image.bash $p
-    git tag $p-image eschwartz/$p-image
-    git push eschwartz/$p-image
+    docker tag $p-image eschwartz/covize:$p
+    docker push eschwartz/covize:$p
 done
